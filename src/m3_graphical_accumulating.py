b1dbc499a82457c77c9ce8266bfb1dbc81f71379
bowmanj1 <bowmanj1@rose-hulman.edu>
2018-09-09 22:07:11 -0400

    j

--- src/m3_graphical_accumulating.py
+++ src/m3_graphical_accumulating.py
@@ -9,8 +9,8 @@ Additionally, it emphasizes that you must
 before you can implement a solution to the problem in Python.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Jacob Bowman.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -32,6 +32,7 @@ def main():
 
 
 def run_test_draw_parallel_lines():
+
     """ Tests the   draw_parallel_lines  function. """
     print()
     print('--------------------------------------------------')
@@ -69,6 +70,9 @@ def run_test_draw_parallel_lines():
 
 
 def draw_parallel_lines(n, point, length, window):
+    for k in range(n + 1):
+        line = rg.Line()
+
     """
     What comes in: The four arguments are:
       -- A positive integer n.
